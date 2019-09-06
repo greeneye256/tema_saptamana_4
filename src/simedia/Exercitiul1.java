24fa77e384af9cba280ce14dded7872ad69cc7ff
greeneye256 <radusimu@gmail.com>
2019-09-06 12:03:25 +0300

    Tema

--- src/simedia/Exercitiul1.java
+++ src/simedia/Exercitiul1.java
@@ -3,24 +3,36 @@ package simedia;
 public class Exercitiul1 {
 
     public static void main(String[] args) {
-        int numar1 = 76000;
+        int numar1 = 2;
         int numar2 = 76561;
         int countNumerePrime = 0;
-        int countDiv = 0;
 
-        for (int i = numar1; i <= numar2; i++) {
-            for (int j = 2; j <= i/2; j++) {  //aici m-am uitat pe net dupa ce am rezolvat problema si am adaugat i/2 in loc de i si atunci nu va dura asa mult calculul
-                if (i%j==0) {
-                    countDiv++;
-                    break;
-                }
-            }
-            if (countDiv==0){
+        if (numar1==2){
+            countNumerePrime++;
+            System.out.println(numar1);
+        }
+
+        if (numar1%2 == 0){
+            numar1++;
+        }
+
+        for (int i = numar1; i <= numar2; i+=2) {
+            if (isPrime(i)){
                 countNumerePrime++;
+                System.out.println(i);
             }
-            countDiv=0;
         }
         System.out.println(countNumerePrime);
+    }
+    public static boolean isPrime(int number){
+        int countDiv = 0;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number%i == 0){
+                countDiv++;
+                break;
+            }
+        }
+        return (countDiv==0);
     }
 
 }
